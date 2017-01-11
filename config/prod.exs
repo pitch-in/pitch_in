c2dcf8cf2485184a4bfe622e9108e6bc15fa3952
Will Ockelmann-Wagner <william.lee.wagner@gmail.com>
2017-01-11 01:28:29 -0800

    fix a couple prod issues

--- config/prod.exs
+++ config/prod.exs
@@ -13,9 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :pitch_in, PitchIn.Endpoint,
   http: [port: {:system, "PORT"}],
-  cache_static_manifest: "priv/static/manifest.json",
-
-
+  cache_static_manifest: "priv/static/manifest.json"
 
 try do
   # Use prod.secret if it exists.
