17af649caeb97aeb210d3e2e2c1cbecd4dedee81
Will Ockelmann-Wagner <william.lee.wagner@gmail.com>
2017-01-11 01:39:22 -0800

    arrrg fix repo config name take 2

--- config/prod.exs
+++ config/prod.exs
@@ -28,7 +28,7 @@ rescue
       force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
     # Configure your database
-    config :pitch_in, HelloPhoenix.Repo,
+    config :pitch_in, PitchIn.Repo,
       adapter: Ecto.Adapters.Postgres,
       url: System.get_env("DATABASE_URL"),
       pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
