19e002b8c14ff541d3b44a09226df76dbf1ff9bd
Will Ockelmann-Wagner <william.lee.wagner@gmail.com>
2017-01-11 02:38:51 -0800

    don't limit static file serving

--- lib/pitch_in/endpoint.ex
+++ lib/pitch_in/endpoint.ex
@@ -8,8 +8,7 @@ defmodule PitchIn.Endpoint do
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
   plug Plug.Static,
-    at: "/", from: :pitch_in, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    at: "/", from: :pitch_in, gzip: true
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
