99c3e2ae1b6554e7fb04a184d7714ef4a869def5
Will Ockelmann-Wagner <william.lee.wagner@gmail.com>
2017-01-11 17:57:36 -0800

    a couple tweeks to build

--- web/static/index.ts
+++ web/static/index.ts
@@ -3,8 +3,6 @@ import 'phoenix';
 import 'phoenix_html';
 import './styles/main.scss';
 import './favicon.ico';
-import './favicon-32x32.png';
-import './favicon-16x16.png';
 const logo = require('./img/logo.png');
 
 console.log("Hello world!");
