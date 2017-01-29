28cbd2478d30d499346bce53443da016e414775a
Will Ockelmann-Wagner <william.lee.wagner@gmail.com>
2017-01-28 16:08:02 -0800

    update host

--- config/prod.exs
+++ config/prod.exs
@@ -24,7 +24,7 @@ rescue
 
     config :pitch_in, PitchIn.Endpoint,
       secret_key_base: System.get_env("SECRET_KEY_BASE"),
-      url: [scheme: "https", host: "pitch-in-app.herokuapp.com", port: 443],
+      url: [scheme: "https", host: "pitch-in.us", port: 443],
       force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
     # Configure your database
