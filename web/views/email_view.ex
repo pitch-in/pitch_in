85eba636467d319a292a909eead6b000d8037680
Will Ockelmann-Wagner <william.lee.wagner@gmail.com>
2017-02-12 02:24:48 -0800

    add full path to activist welcome email

--- web/views/email_view.ex
+++ web/views/email_view.ex
@@ -1,3 +1,12 @@
 defmodule PitchIn.EmailView do
   use PitchIn.Web, :view
+
+  def full_domain(%Plug.Conn{scheme: scheme, host: host, port: port}) do
+    url = "#{scheme}://#{host}"
+    if port do
+      url <> port
+    else
+      url
+    end
+  end
 end
