cf7cc44df24725ddde0fb561b5e5dcfa964066ad
Will Ockelmann-Wagner <william.lee.wagner@gmail.com>
2017-02-11 21:01:29 -0800

    use new logos on nav and home

--- web/static/index.ts
+++ web/static/index.ts
@@ -7,7 +7,9 @@ import './fonts/Phosphate-Solid.ttf';
 import './favicon.ico';
 import './robots.txt';
 import './img/logo.png';
+import './img/logo_216.png';
 import './img/shovels.png';
+import './img/shovels_56.png';
 
 // Expose jquery, for Foundation.
 const $ = require('expose?$!expose?jQuery!jquery');
