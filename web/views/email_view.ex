2aa07394c815a2bcd7e9621da664951701b794c5
Will Ockelmann-Wagner <william.lee.wagner@gmail.com>
2017-04-15 15:59:41 -0700

    fix password_reset_path

--- web/views/email_view.ex
+++ web/views/email_view.ex
@@ -4,6 +4,6 @@ defmodule PitchIn.EmailView do
   use PitchIn.Web, :view
 
   def full_password_reset_path(conn, user) do
-    base_url(@conn) <> forgot_password_path(conn, :index, email: user.email, token: user.reset_token)
+    base_url(conn) <> forgot_password_path(conn, :index, email: user.email, token: user.reset_token)
   end
 end
