67fcd7044bb5a3ff172eba15c141e99bbb63e032
Will Ockelmann-Wagner <william.lee.wagner@gmail.com>
2017-04-23 13:21:34 -0700

    switch to new pheonix version

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule PitchIn.Mixfile do
 
   def project do
     [app: :pitch_in,
-     version: "1.1.0",
-     elixir: "~> 1.2",
+     version: "1.2.0",
+     elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
@@ -35,7 +35,7 @@ defmodule PitchIn.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
+    [{:phoenix, "~> 1.3.0-rc"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.0"},
      {:postgrex, ">= 0.0.0"},
